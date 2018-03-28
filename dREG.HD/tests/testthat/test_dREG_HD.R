4f9c503a6f495208079433d508d80b6cb038c380
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2018-03-28 18:19:24 -0400

    add GPU test

--- dREG.HD/tests/testthat/test_dREG_HD.R
+++ dREG.HD/tests/testthat/test_dREG_HD.R
@@ -11,10 +11,17 @@ bed_path <- bundled_file("k562.chr21.predictions.bed")
 bigwig_plus <- bundled_file("K562.chr21.plus.bw")
 bigwig_minus <- bundled_file("K562.chr21.minus.bw")
 load(bundled_file("dREG_HD.model.rdata"))
+ncores <- parallel::detectCores()
 
 ## Tests.
-test_that("dREG_HD runs null value", {
-    ret <- dREG_HD(bed_path, bigwig_plus, bigwig_minus, model, ncores = 12,
+test_that("dREG_HD CPU returns null value", {
+    ret <- dREG_HD(bed_path, bigwig_plus, bigwig_minus, model, ncores,
                    use_rgtsvm = FALSE)
+    expect_null(ret)
+})
+
+test_that("dREG_HD GPU returns null value", {
+    ret <- dREG_HD(bed_path, bigwig_plus, bigwig_minus, model, ncores,
+                   use_rgtsvm = TRUE)
     expect_null(ret)
 })
